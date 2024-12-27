138744332a8aca7301b935369f38b2a1811e525b
minseok <pms950106@gmail.com>
2024-12-27 15:14:40 +0900

    first commit

---
+++ PokeGuide/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  PokeGuide
+//
+//  Created by t2023-m0072 on 12/27/24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = UINavigationController(rootViewController: MainViewController())
+        window.makeKeyAndVisible()
+        
+        self.window = window
+        
+        return true
+    }
+
+}
+
---
+++ PokeGuide/ViewModel/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokeGuide
+//
+//  Created by t2023-m0072 on 12/27/24.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
